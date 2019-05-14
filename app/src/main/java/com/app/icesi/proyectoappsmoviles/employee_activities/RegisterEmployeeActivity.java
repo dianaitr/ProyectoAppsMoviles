9e05e9db822859498c1eb270b69d790d4e9acad0
Diana Isabel Torres Rojas <diana.torres6@correo.icesi.edu.co>
2019-05-14 16:32:42 -0500

    agrego campos faltantes en registerEmployee

--- app/src/main/java/com/app/icesi/proyectoappsmoviles/employee_activities/RegisterEmployeeActivity.java
+++ app/src/main/java/com/app/icesi/proyectoappsmoviles/employee_activities/RegisterEmployeeActivity.java
@@ -1,9 +1,18 @@
-package com.app.icesi.proyectoappsmoviles;
+package com.app.icesi.proyectoappsmoviles.employee_activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.app.icesi.proyectoappsmoviles.R;
 
 public class RegisterEmployeeActivity extends AppCompatActivity {
+
+    Button btn_register;
+
+    EditText txtName,txtLastName,txtAdress,txtEmail;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
