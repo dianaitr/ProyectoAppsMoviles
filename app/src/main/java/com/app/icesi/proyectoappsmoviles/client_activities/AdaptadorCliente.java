a2343826eac6232ea258e8b4d1abb3eaa11f2f3f
Diana Isabel Torres Rojas <diana.torres6@correo.icesi.edu.co>
2019-05-16 17:09:55 -0500

    Funciona el muro con el adapter

--- app/src/main/java/com/app/icesi/proyectoappsmoviles/client_activities/AdaptadorCliente.java
+++ app/src/main/java/com/app/icesi/proyectoappsmoviles/client_activities/AdaptadorCliente.java
@@ -1,29 +1,39 @@
 package com.app.icesi.proyectoappsmoviles.client_activities;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.app.icesi.proyectoappsmoviles.R;
+import com.app.icesi.proyectoappsmoviles.model.Usuario;
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
 
 public class AdaptadorCliente extends RecyclerView.Adapter<AdaptadorCliente.CustomViewHolder> {
 
-    Context contexto;
+    ArrayList<Usuario> data;
+    FirebaseStorage storage;
 
-    @NonNull
-    @Override
-    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public AdaptadorCliente(ArrayList<Usuario> listaUsuarios){
+        this.data=listaUsuarios;
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull CustomViewHolder customViewHolder, int i) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
+    public void showAllAmigos(ArrayList<Usuario> allUsuarios) {
+        for(int i = 0 ; i<allUsuarios.size() ; i++){
+            if(!data.contains(allUsuarios.get(i))) data.add(allUsuarios.get(i));
+        }
+        notifyDataSetChanged();
     }
 
     public static class CustomViewHolder extends RecyclerView.ViewHolder {
@@ -33,5 +43,60 @@ public class AdaptadorCliente extends RecyclerView.Adapter<AdaptadorCliente.Cust
             root = v;
         }
     }
+
+    public AdaptadorCliente(){
+        data = new ArrayList<>();
+    }
+
+    @Override
+    public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.griditem, parent, false);
+        CustomViewHolder vh = new CustomViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(final CustomViewHolder holder, final int position) {
+        ((TextView) holder.root.findViewById(R.id.renglon_nombreEmpleado)).setText(data.get(position).getNombres());
+        ((TextView) holder.root.findViewById(R.id.renglon_apellidoEmpleado)).setText(data.get(position).getApellidos());
+        holder.root.findViewById(R.id.renglon_empleado_call).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onItemClick(data.get(position));
+            }
+        });
+        ((TextView) holder.root.findViewById(R.id.renglon_calEmpleado)).setText(data.get(position).getCalificacion()+"");
+
+        storage = FirebaseStorage.getInstance();
+        StorageReference ref = storage.getReference().child("usuarios").child("colaboradores").child(data.get(position).getUid());
+        ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                ImageView img = holder.root.findViewById(R.id.renglon_imagenEmpleado);
+                Glide.with(holder.root.getContext()).load(uri).into(img);
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    //OBSERVER
+    public interface OnItemClickListener{
+        void onItemClick(Usuario usuario);
+        //void onChat(Amigo amigo);
+    }
+
+    private OnItemClickListener listener;
+
+    public void setListener(OnItemClickListener listener){
+        this.listener = listener;
+    }
+
+
 
 }
--- app/src/main/java/com/app/icesi/proyectoappsmoviles/client_activities/BuscarServicioActivity.java
+++ app/src/main/java/com/app/icesi/proyectoappsmoviles/client_activities/BuscarServicioActivity.java
@@ -1,65 +1,137 @@
 package com.app.icesi.proyectoappsmoviles.client_activities;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.app.icesi.proyectoappsmoviles.R;
+import com.app.icesi.proyectoappsmoviles.model.Usuario;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class BuscarServicioActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class BuscarServicioActivity extends AppCompatActivity implements  AdaptadorCliente.OnItemClickListener {
 
     private EditText buscador;
-private TextView nombreEmpleado;
-private TextView apellidoEmpelado;
-private TextView calificacionEmpleado;
-private ImageView fotoEmpleado;
-private GridView grid;
-    private BottomNavigationView btn_navigationview2;
+    private TextView nombreEmpleado;
+    private TextView apellidoEmpelado;
+    private TextView calificacionEmpleado;
+    private ImageView fotoEmpleado;
+
+
+    private RecyclerView lvServices;
+
+
+    private AdaptadorCliente adapter;
+
+    FirebaseDatabase rtdb;
+    FirebaseAuth auth;
+
+    ArrayList<Usuario> listaUsuarios;
+
+    ImageButton btn_buscarServicios;
+
+    private BottomNavigationView btn_navigation;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscar_servicio);
-        grid= findViewById(R.id.gv_buscarServicioCliente);
-        fotoEmpleado=findViewById(R.id.imagenEmpleado_cliente);
-        nombreEmpleado=findViewById(R.id.tv_nameEmpleado_cliente);
-        apellidoEmpelado=findViewById(R.id.tv_apellidoEmpleado_cliente);
-        calificacionEmpleado=findViewById(R.id.tv_numeroCalificionEmpleado_cliente);
+
+        rtdb = FirebaseDatabase.getInstance();
+
+        auth = FirebaseAuth.getInstance();
+
+
+        listaUsuarios=new ArrayList<Usuario>();
+
+
+        fotoEmpleado=findViewById(R.id.renglon_imagenEmpleado);
+        nombreEmpleado=findViewById(R.id.renglon_nombreEmpleado);
+        apellidoEmpelado=findViewById(R.id.renglon_apellidoEmpleado);
+        calificacionEmpleado=findViewById(R.id.renglon_calEmpleado);
         buscador=findViewById(R.id.et_buscadorServicioCliente);
-        btn_navigationview2=findViewById(R.id.menu_navigation);
-        btn_navigationview2.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+
+        lvServices=findViewById(R.id.lvServices);
+        lvServices.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter=new AdaptadorCliente(listaUsuarios);
+        lvServices.setAdapter(adapter);
+        adapter.setListener(this);
+
+        lvServices.setHasFixedSize(true);
+        llenarRenglones();
+
+        btn_buscarServicios= (ImageButton) findViewById(R.id.btn_buscarServicios);
+        btn_buscarServicios.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-
-                if (menuItem.getItemId()==R.id.menu_perfilUsuario){
-
-                    Intent i= new Intent(BuscarServicioActivity.this, PerfilClienteActivity.class);
-                    startActivity(i);
-                    finish();
-                }else if(menuItem.getItemId()==R.id.menu_notificaciones){
-                    Intent i= new Intent(BuscarServicioActivity.this, NotificacionClienteActivity.class);
-                    startActivity(i);
-                    finish();
-
-                }
-
-                return false;
+            public void onClick(View v) {
+                //TODO - buscarServicios
             }
         });
 
 
 
 
+    }
 
+    private void llenarRenglones() {
 
+        rtdb.getReference().child("usuarios").child("colaboradores").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                listaUsuarios.clear();
+                for(DataSnapshot objSnapshot: dataSnapshot.getChildren()){
+                    Usuario p= objSnapshot.getValue(Usuario.class);
+                    listaUsuarios.add(p);
 
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //adapter.showAllAmigos(localdb.getAllAmigosOfUser(auth.getCurrentUser().getUid()));
+    }
+
+
+    @Override
+    public void onItemClick(Usuario amigo) {
+        Intent i = new Intent( Intent.ACTION_CALL );
+        //i.setData( Uri.parse("tel:"+amigo.getTelefono()) );
+        startActivity(i);
+    }
+
+
 }
