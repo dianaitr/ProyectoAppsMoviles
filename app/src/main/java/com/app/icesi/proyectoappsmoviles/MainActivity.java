b4cd2fc6261479de52925349debb3eb8c4501380
Valeryibarra <valeryibarra2@gmail.com>
2019-05-28 18:59:50 -0500

    Vista de perfil cliente arreglada navigation

--- app/src/main/java/com/app/icesi/proyectoappsmoviles/MainActivity.java
+++ app/src/main/java/com/app/icesi/proyectoappsmoviles/MainActivity.java
@@ -18,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         btn_employee=findViewById(R.id.btn_employee);
+
         btn_employee.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
