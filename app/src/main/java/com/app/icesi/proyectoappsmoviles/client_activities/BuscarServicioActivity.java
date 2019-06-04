228684b07cef58a26d510b0b30e0b04f9835e6f5
Juan David <juan-bol98@hotmail.com>
2019-06-03 22:49:29 -0500

    Filtro de cercanía listo, aplicado sobre todos los colaboradores, se debería hacer sobre los colaboradores que oferten los servicios requeridos

--- app/src/main/java/com/app/icesi/proyectoappsmoviles/client_activities/BuscarServicioActivity.java
+++ app/src/main/java/com/app/icesi/proyectoappsmoviles/client_activities/BuscarServicioActivity.java
@@ -2,6 +2,8 @@ package com.app.icesi.proyectoappsmoviles.client_activities;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -12,6 +14,7 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +22,7 @@ import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.app.icesi.proyectoappsmoviles.R;
@@ -32,10 +36,19 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class BuscarServicioActivity extends AppCompatActivity implements  AdaptadorCliente.OnItemClickListener {
 
-    private EditText buscador;
+    private Spinner sp_filtro;
     private TextView nombreEmpleado;
     private TextView apellidoEmpelado;
     private TextView calificacionEmpleado;
@@ -53,13 +66,8 @@ public class BuscarServicioActivity extends AppCompatActivity implements  Adapta
 
     ArrayList<Usuario> listaUsuarios;
 
-    ImageButton btn_buscarServicios;
-
     private BottomNavigationView btn_navigation;
 
-
-
-    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,47 +77,95 @@ public class BuscarServicioActivity extends AppCompatActivity implements  Adapta
 
         auth = FirebaseAuth.getInstance();
 
+        final Location myLocation = getMyLocation();
 
-        listaUsuarios=new ArrayList<Usuario>();
-
-
-        fotoEmpleado=findViewById(R.id.renglon_imagenEmpleado);
-        nombreEmpleado=findViewById(R.id.renglon_nombreEmpleado);
-        apellidoEmpelado=findViewById(R.id.renglon_apellidoEmpleado);
-        calificacionEmpleado=findViewById(R.id.renglon_calEmpleado);
-        buscador=findViewById(R.id.et_buscadorServicioCliente);
+        listaUsuarios = new ArrayList<Usuario>();
 
 
-        lvServices=findViewById(R.id.lvServices);
+        fotoEmpleado = findViewById(R.id.renglon_imagenEmpleado);
+        nombreEmpleado = findViewById(R.id.renglon_nombreEmpleado);
+        apellidoEmpelado = findViewById(R.id.renglon_apellidoEmpleado);
+        calificacionEmpleado = findViewById(R.id.renglon_calEmpleado);
+
+        sp_filtro = findViewById(R.id.sp_filtro);
+        String[] opciones = {"Sin filtro","Cercanía", "Calificación"};
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.filter_spinner_row, R.id.filter, opciones);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, opciones);
+        sp_filtro.setAdapter(arrayAdapter);
+
+        sp_filtro.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, final long id) {
+
+                Log.e(">>>","Holiiiii");
+
+                if(position==0){
+                    llenarRenglones();
+                }
+                else if(position==1){
+                    rtdb.getReference().child("usuarios").child("colaboradores").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            listaUsuarios.clear();
+
+                            HashMap<Usuario, Float> distances = new HashMap<Usuario, Float>();
+                            for(DataSnapshot objSnapshot: dataSnapshot.getChildren()){
+
+                                Usuario usuario= objSnapshot.getValue(Usuario.class);
+                                Location userLocation = new Location(LocationManager.NETWORK_PROVIDER);
+                                userLocation.setLatitude(usuario.getLatitude());
+                                userLocation.setLongitude(usuario.getLongitude());
+                                float distance = myLocation.distanceTo(userLocation)/1000;
+                                distances.put(usuario,distance);
+                            }
+
+                            HashMap<Usuario, Float> sortDistances = sortByValue(distances);
+                            Iterator<Usuario> iterator = sortDistances.keySet().iterator();
+                            while(iterator.hasNext()){
+                                listaUsuarios.add(iterator.next());
+                            }
+
+                            adapter.notifyDataSetChanged();
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+
+                }else if (position==2){
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        lvServices = findViewById(R.id.lvServices);
         lvServices.setLayoutManager(new LinearLayoutManager(this));
 
-        adapter=new AdaptadorCliente(listaUsuarios);
+        adapter = new AdaptadorCliente(listaUsuarios);
         lvServices.setAdapter(adapter);
         adapter.setListener(this);
 
         lvServices.setHasFixedSize(true);
-        llenarRenglones();
 
-        btn_buscarServicios= (ImageButton) findViewById(R.id.btn_buscarServicios);
-        btn_buscarServicios.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //TODO - buscarServicios
-            }
-        });
-
-        btn_navigation=findViewById(R.id.btn_navigation);
+        btn_navigation = findViewById(R.id.btn_navigation);
         btn_navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
-                if (menuItem.getItemId()==R.id.menu_perfilUsuario){
+                if (menuItem.getItemId() == R.id.menu_perfilUsuario) {
 
-                    Intent i= new Intent(BuscarServicioActivity.this, PerfilClienteActivity.class);
+                    Intent i = new Intent(BuscarServicioActivity.this, PerfilClienteActivity.class);
                     startActivity(i);
                     finish();
-                }else if(menuItem.getItemId()==R.id.menu_notificaciones){
-                    Intent i= new Intent(BuscarServicioActivity.this, NotificacionClienteActivity.class);
+                } else if (menuItem.getItemId() == R.id.menu_notificaciones) {
+                    Intent i = new Intent(BuscarServicioActivity.this, NotificacionClienteActivity.class);
                     startActivity(i);
                     finish();
 
@@ -119,43 +175,54 @@ public class BuscarServicioActivity extends AppCompatActivity implements  Adapta
             }
         });
 
+    }
 
-        btn_seleccionarEmpleado=findViewById(R.id.renglon_empleado_call);
-        btn_seleccionarEmpleado.setOnClickListener(new View.OnClickListener() {
+    private Location getMyLocation() {
+        final Location myLocation = new Location(LocationManager.NETWORK_PROVIDER);
+        rtdb.getReference().child("usuarios").child("clientes").child(auth.getCurrentUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                DatabaseReference dr= (DatabaseReference) rtdb.getReference().child("colaboradores").child(auth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Usuario usuario = (Usuario) dataSnapshot.getValue(Usuario.class);
+                myLocation.setLatitude(usuario.getLatitude());
+                myLocation.setLongitude(usuario.getLongitude());
+            }
 
-                        String telefonoEmpleado= String.valueOf(dataSnapshot.child("telefono").getValue());
-
-                        if (dataSnapshot.child("telefono").getValue()!=null) {
-
-                            Intent i = new Intent(BuscarServicioActivity.this, PerfilEmpleadoBuscadoActivity.class);
-                            startActivity(i);
-                    }
-
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
-
-
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
-
-
+        return myLocation;
     }
+
+    // function to sort hashmap by values
+    public static HashMap<Usuario, Float> sortByValue(HashMap<Usuario, Float> hm)
+    {
+        // Create a list from elements of HashMap
+        List<Map.Entry<Usuario, Float> > list =
+                new LinkedList<Map.Entry<Usuario, Float> >(hm.entrySet());
+
+        // Sort the list
+        Collections.sort(list, new Comparator<Map.Entry<Usuario, Float> >() {
+            public int compare(Map.Entry<Usuario, Float> o1,
+                               Map.Entry<Usuario, Float> o2)
+            {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        // put data from sorted list to hashmap
+        HashMap<Usuario, Float> temp = new LinkedHashMap<Usuario, Float>();
+        for (Map.Entry<Usuario, Float> aa : list) {
+            temp.put(aa.getKey(), aa.getValue());
+        }
+        return temp;
+    }
+
 
     private void llenarRenglones() {
 
-        rtdb.getReference().child("usuarios").child("colaboradores").addValueEventListener(new ValueEventListener() {
+            rtdb.getReference().child("servicios_ofertados").child("colaboradores").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 listaUsuarios.clear();
